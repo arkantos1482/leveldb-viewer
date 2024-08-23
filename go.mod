09804bace052ecf4550d1a8ccc116cf255157d56
abbas <abbas_mhosseini@yahoo.com>
2024-08-23 14:05:09 +0330

    add graphical user interface

--- go.mod
+++ go.mod
@@ -2,6 +2,19 @@ module github.com/arkantos1482/leveldb-viewer
 
 go 1.22.6
 
-require github.com/syndtr/goleveldb v1.0.0
+require (
+	github.com/rivo/tview v0.0.0-20240818110301-fd649dbf1223
+	github.com/syndtr/goleveldb v1.0.0
+)
 
-require github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
+require (
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell/v2 v2.7.1 // indirect
+	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/term v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,17 +3,24 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/syndtr/goleveldb/leveldb/iterator"
-	"log"
-
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
 	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/syndtr/goleveldb/leveldb/util"
+	"log"
+)
+
+var (
+	pageSize      = 100    // Number of keys to display per page
+	currentPage   = 0      // Current page index
+	filteredKeys  [][]byte // Filtered keys based on search or prefix
+	currentPrefix string   // Current prefix filter
+	showHelp      = false  // Flag to show/hide help window
 )
 
 func main() {
 	// Command-line flags
-	dbPath := flag.String("db", "path/to/your/leveldb", "Path to the LevelDB database")
-	prefix := flag.String("prefix", "", "Prefix to filter keys (optional)")
+	dbPath := flag.String("db", "", "Path to the LevelDB database")
 	flag.Parse()
 
 	// Open the LevelDB database
@@ -22,31 +29,143 @@ func main() {
 		log.Fatal(err)
 	}
 
-	//check if dbpath is not empty
+	// Check if dbPath is not empty
 	if *dbPath == "" {
-		log.Fatal("dbPath is empty")
+		log.Fatal("dbPath is empty, add -db flag to specify the path to the LevelDB database")
 	}
 
 	defer db.Close()
 
-	// Create an iterator with optional prefix
-	var iter iterator.Iterator
-	if *prefix != "" {
-		iter = db.NewIterator(util.BytesPrefix([]byte(*prefix)), nil)
-	} else {
-		iter = db.NewIterator(nil, nil)
+	// Create a new tview application
+	app := tview.NewApplication()
+
+	// Create a list to display keys
+	keyList := tview.NewList()
+	keyList.SetBorder(true).
+		SetTitle("Keys")
+
+	// Create a text view to display the value of the selected key
+	valueList := tview.NewTextView()
+	valueList.SetDynamicColors(true).
+		SetBorder(true).
+		SetTitle("Value")
+
+	// Create an input field for searching and prefix filtering
+	searchBox := tview.NewInputField().
+		SetLabel("Search/Prefix: ").
+		SetFieldWidth(20).
+		SetChangedFunc(func(text string) {
+			currentPrefix = text
+			filterKeys(db, keyList, valueList)
+		})
+
+	// Create a help window
+	helpText := `Use Arrow keys to navigate
+'n' for next page
+'p' for previous page
+'q' to quit
+'h' to toggle this help window`
+	helpWindow := tview.NewTextView().
+		SetDynamicColors(true).
+		SetText(helpText)
+	helpWindow.
+		SetBorder(true).
+		SetTitle("Help")
+
+	// Create a flexbox layout
+	flex := tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(searchBox, 1, 1, false).
+		AddItem(tview.NewFlex().
+			AddItem(keyList, 0, 1, true).
+			AddItem(valueList, 0, 2, false), 0, 1, true).
+		AddItem(tview.NewTextView().SetText("Use Arrow keys to navigate, 'n' for next page, 'p' for previous page, 'h' for help"), 1, 1, false)
+
+	// Populate the initial key list
+	filterKeys(db, keyList, valueList)
+
+	// Set input capture for pagination and help window
+	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		switch event.Rune() {
+		case 'n':
+			nextPage(db, keyList, valueList)
+		case 'p':
+			prevPage(db, keyList, valueList)
+		case 'q', 'Q':
+			app.Stop()
+		case 'h', 'H':
+			showHelp = !showHelp
+			if showHelp {
+				flex.AddItem(helpWindow, 0, 1, false)
+			} else {
+				flex.RemoveItem(helpWindow)
+			}
+		}
+		return event
+	})
+
+	// Set up and run the application
+	if err := app.SetRoot(flex, true).Run(); err != nil {
+		log.Fatal(err)
 	}
+}
+
+// filterKeys filters keys based on the current prefix and repopulates the key list
+func filterKeys(db *leveldb.DB, keyList *tview.List, valueList *tview.TextView) {
+	keyList.Clear()
+	currentPage = 0
+
+	iter := db.NewIterator(util.BytesPrefix([]byte(currentPrefix)), nil)
 	defer iter.Release()
 
-	// Iterate over the database
+	filteredKeys = [][]byte{}
 	for iter.Next() {
 		key := iter.Key()
-		value := iter.Value()
-		fmt.Printf("Key: %s, Value: %s\n", key, value)
+		filteredKeys = append(filteredKeys, append([]byte{}, key...)) // Copy key to avoid reference issues
 	}
 
-	// Check for any iterator errors
 	if err := iter.Error(); err != nil {
 		log.Fatal(err)
+	}
+
+	displayPage(db, keyList, valueList)
+}
+
+// displayPage displays the current page of keys in the key list
+func displayPage(db *leveldb.DB, keyList *tview.List, valueList *tview.TextView) {
+	keyList.Clear()
+
+	start := currentPage * pageSize
+	end := start + pageSize
+	if end > len(filteredKeys) {
+		end = len(filteredKeys)
+	}
+
+	for i := start; i < end; i++ {
+		key := filteredKeys[i]
+		keyList.AddItem(string(key), "", 0, func() {
+			value, err := db.Get(key, nil)
+			if err != nil {
+				valueList.SetText(fmt.Sprintf("[red]Error: %v", err))
+			} else {
+				valueList.SetText(fmt.Sprintf("Key: %s\n\nValue: %s", key, value))
+			}
+		})
+	}
+}
+
+// nextPage moves to the next page of keys
+func nextPage(db *leveldb.DB, keyList *tview.List, valueList *tview.TextView) {
+	if (currentPage+1)*pageSize < len(filteredKeys) {
+		currentPage++
+		displayPage(db, keyList, valueList)
+	}
+}
+
+// prevPage moves to the previous page of keys
+func prevPage(db *leveldb.DB, keyList *tview.List, valueList *tview.TextView) {
+	if currentPage > 0 {
+		currentPage--
+		displayPage(db, keyList, valueList)
 	}
 }
